d705f8017bad8f9f0936a9de6cf7c0528addb919
Starkku <starkku.ppmod@gmail.com>
2019-12-07 18:15:22 +0200

    Increased version.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 [assembly: AssemblyCompany("")]  
 [assembly: AssemblyCopyright("Copyright © Starkku 2017-2019")]  
-[assembly: AssemblyTrademark("")]  
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]  
-[assembly: AssemblyVersion("1.0.9.0")]  
-[assembly: AssemblyFileVersion("1.0.9.0")]  
-[assembly: AssemblyInformationalVersion("1.0.9.0")]  
+[assembly: AssemblyVersion("1.0.9.1")]  
+[assembly: AssemblyFileVersion("1.0.9.1")]  
+[assembly: AssemblyInformationalVersion("1.0.9.1")]  
